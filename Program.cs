1201082ac887807861c84561e12319798b3344bb
Doctor Neuron <doctorneuron1718@gmail.com>
2024-05-12 11:29:53 +0700

    Progress auth

--- Program.cs
+++ Program.cs
@@ -6,12 +6,15 @@ using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 using MediatR;
 using MeWhen.Service.Pipe;
+using MeWhen.Domain.Configuration;
 
 internal class Program
 {
     public static IConfiguration Config { get; private set; } = default!;
+
+    public static Supabase.Interfaces.ISupabaseClient<Supabase.Gotrue.User, Supabase.Gotrue.Session, Supabase.Realtime.RealtimeSocket, Supabase.Realtime.RealtimeChannel, Supabase.Storage.Bucket, Supabase.Storage.FileObject> Supabase { get; private set; } = default!;
     
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddDbContext<MeWhenDBContext>(opt =>
@@ -36,8 +39,19 @@ internal class Program
                 config.RegisterServicesFromAssembly(typeof(Program).Assembly);
                 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorPipeline<,>));
             });
+        
+        var supabaseConfig = Config.GetSection("Supabase:Service").Get<SupabaseConfiguration>() ?? throw new Exception("Supabase service not found");
 
         Config = builder.Configuration;
+        Supabase = await new Supabase.Client(
+            supabaseConfig.URL,
+            supabaseConfig.Key,
+            new()
+            {
+                AutoConnectRealtime = true
+            }
+        ).InitializeAsync();
+
         var app = builder.Build();
 
         app.UseSwagger().UseSwaggerUI();
